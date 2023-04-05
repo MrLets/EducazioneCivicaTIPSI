e0e92f39d1e4d4192eaa5c2e993547187acb8544
Ettore Marcolongo <marcolongoettore04@gmail.com>
2023-04-05 21:33:48 +0200

    funzione uno

---
+++ EducazioneCivicaMarcologno/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EducazioneCivicaMarcologno
+{
+
+class Program
+{
+    static void Main(string[] args)
+    {
+    }
+        static string funzioneuno(){
+                    
+        }
+}
+}
