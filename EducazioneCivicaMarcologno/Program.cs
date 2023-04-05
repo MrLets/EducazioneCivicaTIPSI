49507d1087c4c7283f56006377dd222c3fefa146
Ettore Marcolongo <marcolongoettore04@gmail.com>
2023-04-05 21:34:49 +0200

    funzione 1 eseguibile

--- EducazioneCivicaMarcologno/Program.cs
+++ EducazioneCivicaMarcologno/Program.cs
@@ -9,6 +9,29 @@ class Program
     {
     }
         static string funzioneuno(){
+            
+        Console.WriteLine("Inserisci una stringa:");
+        string s = Console.ReadLine();
+        Console.WriteLine("Inserisci un numero:");
+        int n = int.Parse(Console.ReadLine());
+        string result = "";
+        foreach (char c in s)
+        {
+            if (char.IsLetter(c))
+            {
+                char newChar = (char)(c + n);
+                if (char.IsUpper(c) && newChar > 'Z' || char.IsLower(c) && newChar > 'z')
+                {
+                    newChar = (char)(newChar - 26);
+                }
+                result += newChar;
+            }
+            else
+            {
+                result += c;
+            }
+        }
+        Console.WriteLine(result);
                     
         }
 }
